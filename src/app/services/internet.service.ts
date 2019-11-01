a3b849915403bac48337fd9f4de2c0affc2805c7
Mauricio Rondón <mauriciorondonfajardo@gmail.com>
2019-11-01 15:01:15 -0500

    Mejor de lo que Estava

--- src/app/services/internet.service.ts
+++ src/app/services/internet.service.ts
@@ -11,16 +11,19 @@ export class InternetService {
     return[
       {
         Nombre: 'Plan uno',
+        img: 'assets/images/internet/internet.jpg',
         Descripcion: ' este plan consta de 4 megas de descarga.',
         Costo: '$50.000'
       },
       {
         Nombre: 'Plan dos',
+        img: 'assets/images/internet/internet.jpg',
         Descripcion: ' este plan consta de 5 megas de descarga.',
         Costo: '$60.000'
       },
       {
         Nombre: 'Plan tres',
+        img: 'assets/images/internet/internet.jpg',
         Descripcion: ' este plan consta de 6 megas de descarga.',
         Costo: '$80.000'
       }
@@ -29,6 +32,7 @@ export class InternetService {
 }
 export interface Iinternet {
   Nombre: string;
+  img: string;
   Descripcion: string;
   Costo: string;
 }
